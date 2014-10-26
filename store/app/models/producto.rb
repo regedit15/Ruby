4cc336e5d6a40979e38beaed6eab593d716694af
regedit15 <001regedit15@gmail.com>
2014-10-27 06:22:11 +0800

    se agrega validacion de decimal
    Usando expresiones regulares

--- store/app/models/producto.rb
+++ store/app/models/producto.rb
@@ -1,17 +1,19 @@
 class Producto < ActiveRecord::Base
-  attr_accessible :descripcion, :precio, :titulo, :url_imagen
+	attr_accessible :descripcion, :precio, :titulo, :url_imagen
 
-  validates :titulo, :descripcion, :url_imagen, presence: true
+	validates :titulo, :descripcion, :url_imagen, presence: true
 
-  validates :precio, numericality: {greater_than_or_equal_to: 0.01, message: 'El precio debe ser mayor a 0.01'}
+	validates :precio, numericality: {greater_than_or_equal_to: 0.01, message: 'El precio debe ser mayor a 0.01'}
 
-  validates :titulo, uniqueness: true
+	validates_format_of :precio, :with => /\d*\.\d{2}\z/, message: 'Formato incorrecto. Ej: 100.50'
 
-  validates :url_imagen, allow_blank: true, format: {
-with:
-%r{\.(gif|jpg|png)\Z}i,
-message: 'La URL debe ser de tipo GIF, JPG o PNG.'
-}
+	validates :titulo, uniqueness: true
+
+	validates :url_imagen, allow_blank: true, format: {
+		with:
+		%r{\.(gif|jpg|png)\Z}i,
+		message: 'La URL debe ser de tipo GIF, JPG o PNG.'
+	}
 
 
 end
